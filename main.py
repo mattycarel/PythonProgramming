1971fa1f82dfc511a013d11e0184a485ad263d1b
mattycarel <joemattycarelb@gmail.com>
2023-05-06 08:58:18 +0200

    suppression index.html

---
+++ main.py
@@ -0,0 +1,14 @@
+# commentaire simple 
+
+"""commentaire multiligne
+"""
+#par : 
+#arg :
+        
+if __name__ == '__main__':
+    
+    def myfunction():
+        print('Bonjour')
+        
+myfunction()
+        
