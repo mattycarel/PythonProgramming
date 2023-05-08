6dc56f95f653debc41df5ef21bc924b42bd12e87
mattycarel <joemattycarelb@gmail.com>
2023-05-08 09:56:08 +0200

    ajout d'un nouveau fruit

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ if __name__ == '__main__':
    fruits =['Mangue','Banane','Avocat']
    for f in fruits :
      print(f)
+   print('------nouveau fruit') 
+     
+   fruits.insert(1,'pasteque')  
+   for f in fruits :
+     print(f)
         
